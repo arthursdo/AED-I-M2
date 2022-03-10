2550879f1856ade165813eb1d168dcb064a0b3a3
Arthur Sandim de Oliveira <arthur.soliveira@inf.ufpel.edu.br>
2022-03-09 21:34:22 -0300

    Primeiro Commit

---
+++ Semana_1/Origem.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (i % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        if (i % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+}
