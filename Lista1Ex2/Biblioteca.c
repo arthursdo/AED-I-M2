4a33ed52e04daf02ad0322c6b66546601d3de71f
Arthur Sandim de Oliveira <arthur.soliveira@inf.ufpel.edu.br>
2022-03-16 22:47:01 -0300

    Update removernome

--- Lista1Ex2/Biblioteca.c
+++ Lista1Ex2/Biblioteca.c
@@ -52,20 +52,59 @@ char *AdicionarNome(char *nomes)
 
 char *RemoverNome(char *nomes)
 {
-    printf("RemoverNome\n");
+    int opt,busca=0,PosicaoFinal,PossicaoInicial;
+    printf("Qual nome voce gostaria de remover?");
+    Listar(nomes);
+    scanf("%d",&opt);
+
+    if(nomes[0] == '\0'){
+        printf("Lista vazia!\n");
+        return nomes;
+    }
+
+    int i=0;
+    do{
+        if(nomes[i++]=='$'){
+            busca++;
+        }
+    } while (busca!=opt);
+
+    char *tempFim=(char*) malloc(sizeof(char)*(strlen(nomes)-i)+2);
+    tempFim=&nomes[i];
+    //ate aqui funciona
+    do{
+        i--;
+        if(nomes[i]=='$'){
+            nomes[i]='\0';
+        }
+    } while (nomes[i]!='$');
+
+    char *tempIni=(char*) malloc(sizeof(char)*(strlen(nomes)));
+    tempIni=nomes;
+
+    nomes=(char*) malloc((strlen(tempIni)+ strlen(tempFim))*sizeof(char));
+    nomes[0]='\0';
+    strcat(nomes,tempIni);
+    strcat(nomes,tempFim);
+    return nomes;
 }
 void Listar(char *nomes)
 {
     if(nomes[0] == '\0'){
         printf("Lista vazia!\n");
     }else{
-        printf("Nomes:\n");
+        int pos=1;
+        printf("Nomes:\n%d - ",pos);
         for(int i=0;nomes[i]!='\0';i++){
             if(nomes[i]!='$'){
                 printf("%c",nomes[i]);
             }
             else{
-                printf("\n");
+                if(nomes[i+1]=='\0'){
+                    printf("\n");
+                }else{
+                    printf("\n%d - ",++pos);
+                }
             }
 
         }
